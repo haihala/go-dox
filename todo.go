c8a8694bbcbbb1d0b6e97762a3fbe1521f79f7e6
Eero H??ih??l?? <eero.haihala@gmail.com>
2023-09-22 09:32:04 +0300

    Add ability to remove todos

--- todo.go
+++ todo.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 	"sync"
 
 	"github.com/go-chi/chi/v5"
@@ -12,17 +13,17 @@ import (
 )
 
 type Todo struct {
-	Key  int32
+	Key  int
 	Text string
 }
 
 type Todos struct {
 	Value   []Todo
-	Counter int32
+	Counter int
 	Mu      sync.Mutex
 }
 
-func (todos *Todos) Add(text string) int32 {
+func (todos *Todos) Add(text string) int {
 	todos.Mu.Lock()
 	todos.Counter++
 	todos.Value = append(todos.Value, Todo{Text: text, Key: todos.Counter})
@@ -30,7 +31,7 @@ func (todos *Todos) Add(text string) int32 {
 	return todos.Counter
 }
 
-func (todos *Todos) Remove(id int32) {
+func (todos *Todos) Remove(id int) {
 	todos.Mu.Lock()
 	for i, todo := range todos.Value {
 		if todo.Key == id {
@@ -55,15 +56,25 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, _ := template.ParseFiles("index.html")
-		tmpl.ExecuteTemplate(w, "index.html", todos.GetValue())
+		tmpl.Execute(w, todos.GetValue())
 	})
 
 	r.Post("/add", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		field := r.Form.Get("new-todo")
 		key := todos.Add(field)
+		template_data := map[string]string{
+			"Id":      fmt.Sprint(key),
+			"Content": field,
+		}
 
-		fmt.Fprintf(w, "<li id=\"todo-%d\">%s</li>", key, field)
+		tmpl, _ := template.ParseFiles("test.html")
+		tmpl.Execute(w, template_data)
+	})
+
+	r.Post("/remove/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+		todos.Remove(id)
 	})
 
 	fmt.Println("Starting server")
