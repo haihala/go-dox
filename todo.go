2277adcc678689c5ca59a0d97f63c12dd1ca39d0
Eero H??ih??l?? <eero.haihala@gmail.com>
2023-09-22 08:54:55 +0300

    Transform counter to basic todo list

--- todo.go
+++ todo.go
@@ -11,54 +11,50 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-type Counter struct {
-	value int
-	mu    sync.Mutex
+type Todo struct {
+	Text string
 }
 
-func (c *Counter) Increase() {
-	c.mu.Lock()
-	c.value++
-	c.mu.Unlock()
+type Todos struct {
+	Value []Todo
+	Mu    sync.Mutex
 }
 
-func (c *Counter) Decrease() {
-	c.mu.Lock()
-	c.value--
-	c.mu.Unlock()
+func (todos *Todos) Add(todo Todo) {
+	todos.Mu.Lock()
+	todos.Value = append(todos.Value, todo)
+	todos.Mu.Unlock()
 }
 
-func (c *Counter) GetValue() map[string]int {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	return map[string]int{
-		"CounterValue": c.value,
+func (data *Todos) GetValue() map[string][]Todo {
+	data.Mu.Lock()
+	defer data.Mu.Unlock()
+	return map[string][]Todo{
+		"Todos": data.Value,
 	}
 }
 
-func RenderCounter(w http.ResponseWriter, data map[string]int) {
-	tmplStr := "<div id=\"counter\">{{.CounterValue}}</div>"
-	tmpl := template.Must(template.New("counter").Parse(tmplStr))
-	tmpl.ExecuteTemplate(w, "counter", data)
-}
-
 func main() {
-	counter := &Counter{}
+	todos := &Todos{
+		Value: []Todo{
+			{Text: "Test1"},
+			{Text: "Test2"},
+		},
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, _ := template.ParseFiles("index.html")
-		tmpl.ExecuteTemplate(w, "index.html", counter.GetValue())
+		tmpl.ExecuteTemplate(w, "index.html", todos.GetValue())
 	})
 
-	r.Post("/increase", func(w http.ResponseWriter, r *http.Request) {
-		counter.Increase()
-		RenderCounter(w, counter.GetValue())
-	})
+	r.Post("/add", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		field := r.Form.Get("new-todo")
+		todos.Add(Todo{Text: field})
 
-	r.Post("/decrease", func(w http.ResponseWriter, r *http.Request) {
-		counter.Decrease()
-		RenderCounter(w, counter.GetValue())
+		fmt.Fprintf(w, "<li>%s</li>", field)
 	})
 
 	fmt.Println("Starting server")
