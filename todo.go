2209e1f599319f344b1684ca00537eaf3be7fc01
Eero H??ih??l?? <eero.haihala@gmail.com>
2023-09-22 09:45:40 +0300

    Make use of recursive templates
    Clean up while at it

--- todo.go
+++ todo.go
@@ -18,24 +18,24 @@ type Todo struct {
 }
 
 type Todos struct {
-	Value   []Todo
+	Items   []Todo
 	Counter int
 	Mu      sync.Mutex
 }
 
-func (todos *Todos) Add(text string) int {
+func (todos *Todos) Add(text string) Todo {
 	todos.Mu.Lock()
 	todos.Counter++
-	todos.Value = append(todos.Value, Todo{Text: text, Key: todos.Counter})
+	todos.Items = append(todos.Items, Todo{Text: text, Key: todos.Counter})
 	defer todos.Mu.Unlock()
-	return todos.Counter
+	return todos.Items[len(todos.Items)-1]
 }
 
 func (todos *Todos) Remove(id int) {
 	todos.Mu.Lock()
-	for i, todo := range todos.Value {
+	for i, todo := range todos.Items {
 		if todo.Key == id {
-			todos.Value = append(todos.Value[:i], todos.Value[i+1:]...)
+			todos.Items = append(todos.Items[:i], todos.Items[i+1:]...)
 		}
 	}
 	todos.Mu.Unlock()
@@ -45,7 +45,7 @@ func (data *Todos) GetValue() map[string][]Todo {
 	data.Mu.Lock()
 	defer data.Mu.Unlock()
 	return map[string][]Todo{
-		"Todos": data.Value,
+		"Todos": data.Items,
 	}
 }
 
@@ -55,21 +55,17 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, _ := template.ParseFiles("index.html")
-		tmpl.Execute(w, todos.GetValue())
+		tmpl, _ := template.ParseFiles("index.html", "todo.html")
+		tmpl.ExecuteTemplate(w, "index.html", todos.GetValue())
 	})
 
 	r.Post("/add", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		field := r.Form.Get("new-todo")
-		key := todos.Add(field)
-		template_data := map[string]string{
-			"Id":      fmt.Sprint(key),
-			"Content": field,
-		}
+		todo := todos.Add(field)
 
-		tmpl, _ := template.ParseFiles("test.html")
-		tmpl.Execute(w, template_data)
+		tmpl, _ := template.ParseFiles("todo.html")
+		tmpl.ExecuteTemplate(w, "todo", todo)
 	})
 
 	r.Post("/remove/{id}", func(w http.ResponseWriter, r *http.Request) {
